b34ec88e36a33d7124bb1451ba136b015e9b4f5d
alekelbar <alekelbar.developer@gmail.com>
2022-05-11 15:40:04 -0600

    se actualizo el README para colaboradores

--- backend/db/schema.js
+++ backend/db/schema.js
@@ -3,7 +3,7 @@ const { gql } = require("apollo-server");
 const typeDefs = gql`
   #Appendix --------------------------------------------------------------------
   #Appendix definition
-  
+
   type Appendix {
     description: String
     owner: ID
